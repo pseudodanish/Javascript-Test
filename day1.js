b58aff072c37e06a6471a4a6fcaac53eab8b5730
Danish Momin <144316616+pseudodanish@users.noreply.github.com>
2024-04-29 15:40:21 +0530

    Understood about comments

---
+++ day1.js
@@ -0,0 +1,3 @@
+console.log("This Day of Exeercise allows one to understand about comments in the code why they are usefule to us and make our code readable.")
+
+//This is how you write a comment in javascript.
